a5273cad507350c6269a39a4c4642bbb63a5a251
pierre.com <47568177+pierre-com@users.noreply.github.com>
2019-04-16 17:35:11 +0200

    Update arete.cpp

--- piscine/arete.cpp
+++ piscine/arete.cpp
@@ -1,7 +1,7 @@
 #include "arete.h"
 #include <iostream>
 
-Arete::Arete(std::string id_arete, std::string sommet_x, std::string sommet_y, float poids1,float poids2):m_id_arete{id_arete},m_sommet_x{sommet_x},m_sommet_y{sommet_y},m_poids1{1},m_poids2{1} {}
+Arete::Arete(std::string id_arete, std::string sommet_x, std::string sommet_y, float poids1,float poids2):m_id_arete{id_arete},m_sommet_x{sommet_x},m_sommet_y{sommet_y},m_poids1{poids1},m_poids2{poids2} {}
 float Arete::getPoids1()
 {
     return m_poids1;
