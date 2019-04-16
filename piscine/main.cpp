37bc74bf4001b175744bed8145676d167c08b1e7
pierre.com <47568177+pierre-com@users.noreply.github.com>
2019-04-16 15:52:54 +0200

    Update main.cpp

--- piscine/main.cpp
+++ piscine/main.cpp
@@ -5,19 +5,9 @@
 
 int main()
 {
-
-
-    Svgfile svgout;
-    Graphe a("broadway.txt");
-    //std::cout<<"fichier 1 ouvert"<<std::endl;
-    a.ponderation("broadway_weights_0.txt");
-    //std::cout<<"fichier 2 ouvert"<<std::endl;
-
-    a.affichage(&svgout);
-
-//Svgfile svgout;
-    // a.affichage_graphe_1();
-    //svgout.addDisk(100, 100, 90, "greyball");
-    //svgout.addText(300, 300, "25;12", "green");
-    return 0;
+  Svgfile svgout;
+  Graphe a("broadway.txt");
+  a.ponderation("broadway_weights_0.txt");
+  a.affichage(&svgout);
+  return 0;
 }
